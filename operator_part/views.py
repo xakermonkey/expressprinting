37920a81b1edb28efed61e9e8509b7f8a7650f49
Dima <ya.gud@mail.ru>
2021-12-16 23:10:09 +0300

    фулл комплект

--- operator_part/views.py
+++ operator_part/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import *
+from datetime import datetime
 
+def operatorForm(request, pk):
+    pos = POS.objects.get(id=pk)
+    orders = Order.objects.filter(pos=pos)
+    return render(request, 'operator_form.html', {'orders': orders, 'pos':pos})
 
-def operatorForm(request):
-    return render(request, 'operator_form.html')
-
-def orderDetails(request):
-    return render(request, 'order_details.html')
+def orderDetails(request, pk, date, num):
+    order = Order.objects.get(pos=POS.objects.get(pk=pk),
+                              date_create=datetime(day=int(date.split("-")[0]),
+                                                   month=int(date.split("-")[1]),
+                                                   year=int(date.split("-")[2])),
+                              number=num)
+    return render(request, 'order_details.html', {'order': order})
